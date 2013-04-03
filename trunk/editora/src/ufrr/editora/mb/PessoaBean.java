b2daa24efa6c7563e0b16be77484de5f09c3cc5d
leoholanda23@gmail.com <leoholanda23@gmail.com@ce1a04de-fe6c-512b-6fbd-31a64493b013>
2013-04-03 17:13:38 +0000

    update classes LoginBean, UsuarioBean e PessoaBean

--- trunk/editora/src/ufrr/editora/mb/PessoaBean.java
+++ trunk/editora/src/ufrr/editora/mb/PessoaBean.java
@@ -1,6 +1,7 @@
 package ufrr.editora.mb;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
@@ -21,58 +22,70 @@ public class PessoaBean implements Serializable {
 	private Pessoa pessoa = new Pessoa();
 	private Usuario usuario = new Usuario();
 	private Endereco endereco = new Endereco();
+	private List<Usuario> usuarios;
 	private List<Pessoa> pessoas;
+	private List<Pessoa> usuariosNulls;
 	private DAO<Pessoa> dao = new DAO<Pessoa>(Pessoa.class);
 	
 	public Boolean cadastro = true;
 	
+	/** List of peoples **/
+	public List<Pessoa> getPessoas() {
+		if (pessoas == null) {
+			System.out.println("Carregando pessoas...");
+			pessoas = new DAO<Pessoa>(Pessoa.class).getAllOrder("nome");
+		}
+		return pessoas;
+	}
+	
+	// Exibe uma lista com o usuario de status null
+	public List<Pessoa> getUsuarioNulls() {
+		usuariosNulls = new ArrayList<Pessoa>();
+		for (Pessoa p : this.getPessoas()) {
+			if (p.getId() == 1) {
+				usuariosNulls.add(p);
+			}
+		}
+		return usuariosNulls;
+	}
+	
 	//solicitacao de cadastro
 	public String addPessoa() {
-//		for (Pessoa pessoas : this.getPessoas()) {
-//			if(pessoas.getCpf().equalsIgnoreCase(this.getPessoa().getCpf())) { 
-//				this.cadastro = false;
-//				break;
-//			}
-//		}
-		if(this.cadastro == true){
-			if(this.pessoa.getUsuario().getLogin().isEmpty() && this.pessoa.getUsuario().getLogin().length() > 10 ){
-				Msg.addMsgError("Seu login será seu email, informe um email correto para proseguir com o cadastro");
-			}	
-		if(this.cadastro == true){
-			if (getUsuario().getSenha().equals(this.getUsuario().getRepetirSenha())) {
+		for (Pessoa pessoas : this.getPessoas()) {
+			if (pessoas.getCpf().equalsIgnoreCase(this.getPessoa().getCpf())) {
+				this.cadastro = false;
+				break;
+			}
+		}
+
+		if (this.cadastro == true) {
+			if (getUsuario().getSenha().equalsIgnoreCase(this.getUsuario().getRepetirSenha())) {
+//				pessoa.getUsuario().getPerfil().setId((long) 1);
 				pessoa.setUsuario(usuario);
 				pessoa.setEndereco(endereco);
 				dao.adiciona(pessoa);
-				Msg.addMsgInfo("Solicitação de cadastro enviada com sucesso. Aguarde ativação!");
+				Msg.addMsgInfo("Solicitação de acesso enviada com sucesso. Aguarde autorização!");
 				this.pessoa = new Pessoa();
 				this.usuario = new Usuario();
+				System.out.println("...Solicitação enviada");
 				return "index.xhtml";
 			} else {
-				Msg.addMsgError("Senhas diferentes. Por favor, verifique os campos e digite novamente.");
+				System.out.println("...Senhas diferentes");
+				Msg.addMsgError("Senhas diferentes, tente de novo");
 			}
 		} else {
-			if(this.usuario.getLogin() == null){
-				Msg.addMsgError("Seu login será seu email, informe um email para proseguir com o cadastro");
-			} else {
-				Msg.addMsgError("Este cadastro já existe");
-			}
-			return "solicitacao.xhtml";
-			}
+			System.out.println("...cadastro existente");
+			Msg.addMsgError("Este cadastro já existe");
 		}
-		return "solicitacao.xhtml";
+		pessoas = dao.getAllOrder("nome");
+		this.cadastro = true;
+		return null;
 	}
 	
 	//atualiza usuário
 	public String updatePessoa() {
-//		for (Pessoa pessoas : this.getPessoas()) {
-//			if(pessoas.getCpf().equalsIgnoreCase(this.getPessoa().getCpf())) { 
-//				this.cadastro = false;
-//				break;
-//			}
-//		}
-		if(this.cadastro == true){
-			if(this.pessoa.getUsuario().getLogin().isEmpty() && this.pessoa.getUsuario().getLogin().length() > 10 ){
-				Msg.addMsgError("Seu login será seu email, informe um email correto para proseguir com o cadastro");
+		if(this.pessoa.getUsuario().getLogin().isEmpty() && this.pessoa.getUsuario().getLogin().length() > 10 ){
+			Msg.addMsgError("Seu login será seu email, informe um email correto para proseguir com o cadastro");
 			}	
 		if(this.cadastro == true){
 			if (getUsuario().getSenha().equals(this.getUsuario().getRepetirSenha())) {
@@ -92,9 +105,9 @@ public class PessoaBean implements Serializable {
 			}
 			return "solicitacao.xhtml";
 			}
+		return null;
 		}
-		return "solicitacao.xhtml";
-	}
+
 	
 		
 	/** get and set **/
@@ -123,10 +136,6 @@ public class PessoaBean implements Serializable {
 		this.usuario = usuario;
 	}
 
-	public List<Pessoa> getPessoas() {
-		return pessoas;
-	}
-
 	public void setPessoas(List<Pessoa> pessoas) {
 		this.pessoas = pessoas;
 	}
@@ -147,8 +156,22 @@ public class PessoaBean implements Serializable {
 		this.cadastro = cadastro;
 	}
 
-	
-	
+	public List<Usuario> getUsuarios() {
+		return usuarios;
+	}
+
+	public void setUsuarios(List<Usuario> usuarios) {
+		this.usuarios = usuarios;
+	}
+
+	public List<Pessoa> getUsuariosNulls() {
+		return usuariosNulls;
+	}
+
+	public void setUsuariosNulls(List<Pessoa> usuariosNulls) {
+		this.usuariosNulls = usuariosNulls;
+	}
+
 	
 
 }
