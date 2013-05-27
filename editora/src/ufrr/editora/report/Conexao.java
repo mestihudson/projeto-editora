53b8440e856ea77ed53883ef721ffaa94c18aa20
leoholanda23 <leoholanda23@gmail.com>
2013-05-27 19:18:47 +0000

    package jasper ireport

---
+++ editora/src/ufrr/editora/report/Conexao.java
@@ -0,0 +1,27 @@
+package ufrr.editora.report;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+		
+		public static Connection getConexao() {
+	        Connection connection = null;
+	        String url = "org.postgresql.Driver";
+	        String bd = "jdbc:postgresql://localhost:5433/editora_teste";
+	        String usuario = "postgres";
+	        String senha = "admin";
+	        try {
+	            Class.forName(url);
+	            connection = DriverManager.getConnection(bd, usuario, senha);
+	            return connection;
+	        }catch (SQLException e) {
+	            e.printStackTrace();
+	        }catch(ClassNotFoundException e) {
+	            e.printStackTrace();
+	        }
+	        return null;
+	    }
+
+	}
---
+++ editora/src/ufrr/editora/report/Report.java
@@ -0,0 +1,171 @@
+package ufrr.editora.report;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.faces.FacesException;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletResponse;
+
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JRExporter;
+import net.sf.jasperreports.engine.JRExporterParameter;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.export.JRPdfExporter;
+
+public class Report {
+	
+	HashMap<String, Object> parameters;
+	FacesContext facesContext;
+	String nome;
+	Connection con;
+
+	public Report(String nome, HashMap<String, Object> params) {
+		facesContext = FacesContext.getCurrentInstance();
+		this.nome = nome;
+		parameters = new HashMap<String, Object>();
+		parameters.putAll(params);
+	}	
+	
+	public Report() {
+		facesContext = FacesContext.getCurrentInstance();		
+	}
+
+	/**
+	 * Devolve um relatório PDF na response.
+	 */
+	public void pdfReport() {
+		JRExporter exporter = null;
+		exporter = new JRPdfExporter();
+		JasperPrint jasperPrint = getJasperPrint();
+		fillReport("PDF_MIME", exporter, jasperPrint);
+	}
+
+	/**
+	 * A partir do to tipo do relatório, do Exporter e do JasperPrint devolver o
+	 * OutputStream do relatório no OutPutStream da Response.
+	 * 
+	 * @param tipo MIME TYPE do relatório. Será utilizado para configurar o MIME TYPE da Response.
+	 * @param exporter Exporter específico para cada tipo de relatório.
+	 * @param jasperPrint JasperPrint que irá gerar o relatório.
+	 */
+	private void fillReport(String tipo, JRExporter exporter, JasperPrint jasperPrint) {
+
+		ExternalContext econtext = facesContext.getExternalContext();
+
+		HttpServletResponse response = (HttpServletResponse) econtext.getResponse();
+		response.setContentType("application/pdf");  
+		response.setHeader("Content-disposition", "attachment;filename="+nome+".pdf"); 
+
+		try {
+			exporter.setParameter(JRExporterParameter.JASPER_PRINT, jasperPrint);
+
+			exporter.setParameter(JRExporterParameter.OUTPUT_STREAM, response.getOutputStream());
+
+			exporter.exportReport();			
+
+		} catch (RuntimeException ex) {
+			ex.printStackTrace();
+		} catch (JRException ex) {
+			ex.printStackTrace();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+		// Tell JavaServer Faces that no output is required
+		facesContext.responseComplete();
+	}
+
+	/**
+	 * Obtém um objeto JasperPrint para ser utilizado na configuração do
+	 * Exporter de relatórios.
+	 * 
+	 * @return JasperPrint que será utilizado na configuração do Exporter.
+	 */
+	private JasperPrint getJasperPrint() {
+
+		InputStream stream = null;
+		Connection conn = null;
+		JasperPrint jasperPrint = null;
+
+		try {
+			stream = findReport();
+			conn = Conexao.getConexao();			
+			jasperPrint = JasperFillManager.fillReport(stream, parameters, conn);
+		} catch (RuntimeException ex) {
+			ex.printStackTrace();
+			throw ex;
+		} catch (JRException ex) {
+			ex.printStackTrace();
+			throw new FacesException(ex);
+		} finally {
+			try {
+				if (stream != null) {
+					stream.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			} catch (SQLException ex) {
+				ex.printStackTrace();
+			}
+		}
+		return jasperPrint;
+	}
+
+	/**
+	 * Localiza o arquivo .jasper através do nome do relatório.
+	 * 
+	 * @return ImputStream contendo o arquivo .jasper
+	 */
+	@SuppressWarnings("unused")
+	private InputStream findReport() {
+		InputStream retValue = null;
+		try {
+			ServletContext servletContext = (ServletContext) facesContext.getExternalContext().getContext();
+			String realPath = servletContext.getRealPath("/WEB-INF/classes/ufrr/editora/report/" + nome + ".jasper");
+			retValue = new FileInputStream(realPath);
+
+			if (retValue == null) {
+				throw new IllegalArgumentException("O relatório de nome "	+ nome + " não foi encontrado.");
+			}
+		} catch (FileNotFoundException ex) {
+			Logger.getLogger(Report.class.getName()).log(Level.SEVERE, null, ex);
+		}
+		return retValue;
+	}
+	
+	public String findSubReport(String nomeSub) {
+		String realPath2 = "";
+		try {
+			ServletContext servletContext = (ServletContext) facesContext.getExternalContext().getContext();
+			realPath2 = servletContext.getRealPath("/WEB-INF/classes/ufrr/editora/report/" + nomeSub + ".jasper");			
+		} catch (Exception ex) {
+			Logger.getLogger(Report.class.getName()).log(Level.SEVERE, null, ex);
+		}
+		return realPath2;
+	}
+	
+	public String findLogo() {
+		String realPath2 = "";
+		try {
+			ServletContext servletContext = (ServletContext) facesContext.getExternalContext().getContext();
+			realPath2 = servletContext.getRealPath("/img/logo.jpg");			
+		} catch (Exception ex) {
+			Logger.getLogger(Report.class.getName()).log(Level.SEVERE, null, ex);
+		}
+		return realPath2;
+	}
+
+}
