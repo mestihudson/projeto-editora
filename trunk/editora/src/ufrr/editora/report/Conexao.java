4b3bff15727fa1140dd3982f549edc7b3d31718f
leoholanda23@gmail.com <leoholanda23@gmail.com@ce1a04de-fe6c-512b-6fbd-31a64493b013>
2013-07-17 19:24:15 +0000

    servidor de teste concluído...

--- trunk/editora/src/ufrr/editora/report/Conexao.java
+++ trunk/editora/src/ufrr/editora/report/Conexao.java
@@ -6,12 +6,31 @@ import java.sql.SQLException;
 
 public class Conexao {
 		
+//		public static Connection getConexao() {
+//	        Connection connection = null;
+//	        String url = "org.postgresql.Driver";
+//	        String bd = "jdbc:postgresql://localhost:5432/editora";
+//	        String usuario = "postgres";
+//	        String senha = "leo123";
+//	        try {
+//	            Class.forName(url);
+//	            connection = DriverManager.getConnection(bd, usuario, senha);
+//	            return connection;
+//	        }catch (SQLException e) {
+//	            e.printStackTrace();
+//	        }catch(ClassNotFoundException e) {
+//	            e.printStackTrace();
+//	        }
+//	        return null;
+//	    }
+		
+		// para o servidor de teste
 		public static Connection getConexao() {
 	        Connection connection = null;
 	        String url = "org.postgresql.Driver";
-	        String bd = "jdbc:postgresql://localhost:5433/editora_teste";
+	        String bd = "jdbc:postgresql://localhost:5432/SIS_editora";
 	        String usuario = "postgres";
-	        String senha = "admin";
+	        String senha = "dticsi@ufrr";
 	        try {
 	            Class.forName(url);
 	            connection = DriverManager.getConnection(bd, usuario, senha);
